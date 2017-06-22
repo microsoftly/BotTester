a1235dcf5561eeb6f457807b7422484d2bdd733a
Matt Schwartz <mattschwartz310@gmail.com>
2017-06-21 18:27:16 -0700

    added custom step build example

--- docs/exampleUse.spec.js
+++ docs/exampleUse.spec.js
@@ -109,5 +109,81 @@ describe('BotTester Usage', () => {
             })
         ])
     })
+
+    it('Can allow for custom DialogStepDefinitions', () => {
+        bot.dialog('/', [(session) => {
+            new builder.Prompts.text(session, 'What would you like to set data to?')
+        }, (session, results) => {
+            session.userData = { data: results.response };
+            session.save();
+        }]);
+
+        const {
+            executeDialogTest,
+
+            // Utility functions to allow for custom DialogSteps
+            getSession,
+            sendMessageToBot,
+            setBotToUserMessageChecker,
+        } = BotTester(bot);
+
+        // A DialogStep is a class that has an execute function that returns a promise whent he check is done
+        class GreetingStep {
+            execute() {
+                return new Promise((res, rej) => {
+                    // an array is always passed to this function, even if there is only one message in the response
+                    // this is the case because the bot can send messages in batches
+                    setBotToUserMessageChecker((messages) => {
+                        expect(messages.length).to.equal(1);
+                        const message = messages[0];
+
+                        expect(message.text).to.equal('What would you like to set data to?')
+                        res();
+                    })
+
+                    // send message to the bot. Any message returned will be sent to the botToUserMessageChecker
+                    // set above. The execute step should only resolve once the message checker has confirmed 
+                    // a proper value has been returned.
+                    sendMessageToBot('Greetings!')
+                })
+            }
+        }
+
+        class SaySevenStep {
+            execute() {
+                return new Promise((res, rej) => {
+                    setBotToUserMessageChecker((messages) => {
+                        expect(messages.length).to.equal(1);
+                        const message = messages[0];
+
+                        // In the case that a bot does not respond, but session state is changed, i.e. session.save()
+                        // is called, the bot responds to the user with a message with no text whose type is 'save'
+                        // this lets the test/user know that the session save has completed.
+
+                        // If session.save() is called multiple times, then this function will have to know how many times
+                        // it is called before resolving. This is a likely edge case that you will run into
+                        expect(message.type).to.equal('save');
+                        res()
+                    })
+
+                    // send message to the bot.
+                    sendMessageToBot('seven')
+                })
+            }
+        }
+
+        class EnsureUserDataHasSevenStep {
+            execute() {
+                return getSession()
+                    .then((session) => expect(session.userData.data).to.equal('seven'));
+            }
+        }
+
+        return executeDialogTest([
+            new GreetingStep(),
+            new SaySevenStep(),
+            new EnsureUserDataHasSevenStep()
+        ]);
+    })
 }) 
 
