0a050c72ec98771657fd456455a7b812879d7e54
Matthew Schwartz <mattschwartz310@gmail.com>
2018-01-25 08:58:58 -0800

    [feature] assertion library selection by config/options (#59)

--- src/assertionLibraries/IExpectation.ts
+++ src/assertionLibraries/IExpectation.ts
@@ -1,5 +1,3 @@
-import { ChaiExpectation } from './ChaiExpectation';
-
 /**
  * framework based extractions. New frameworks will implement this interface to be able to be used seamlessly within the framework
  */
@@ -9,25 +7,4 @@ export interface IExpectation {
     toInclude(value: {}): void;
     toBeTrue(): void;
     toDeeplyInclude(value: {}): void;
-}
-
-/**
- * Creates a wrapper instance around an assertion class
- *
- * @param subject object being tested against
- * @param message error message to print if expectation fails
- */
-export function expect(subject: {}, message?: string): IExpectation {
-    return createExpectation(subject, message);
-}
-
-function createExpectation(subject: {}, message?: string): IExpectation {
-    const name = 'chai';
-
-    switch (name) {
-        case 'chai':
-            return new ChaiExpectation(subject, message);
-        default:
-            return new ChaiExpectation(subject, message);
-    }
 }
